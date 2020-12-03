28feeeb6bfd3d604edecd95414cd7d57f74b50eb
Lucaz Lindgren <lucaz.lindgren@elev.ga.lbs.se>
2020-12-03 20:08:25 +0100

    Update

--- DigDig Project/Assets/Scripts/CameraManager.cs
+++ DigDig Project/Assets/Scripts/CameraManager.cs
@@ -2,26 +2,15 @@
 
 public class CameraManager : MonoBehaviour
 {
-    #region Variables
-
     public GameObject vCam;
-
-
-    #endregion
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player") && !other.isTrigger)
-        {
-            vCam.SetActive(true);
-        }
+        if(other.CompareTag("Player") && !other.isTrigger) vCam.SetActive(true);
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other) 
     {
-        if (other.CompareTag("Player") && !other.isTrigger)
-        {
-            vCam.SetActive(false);
-        }
+        if (other.CompareTag("Player") && !other.isTrigger) vCam.SetActive(false);
     }
 }
