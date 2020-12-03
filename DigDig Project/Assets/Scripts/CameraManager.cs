b67832c963eab53ec404ff21ae2f825330c59c74
Lucaz Lindgren <lucaz.lindgren@elev.ga.lbs.se>
2020-12-03 19:48:38 +0100

    Working

---
+++ DigDig Project/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    #region Variables
+
+    public GameObject vCam;
+
+
+    #endregion
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            vCam.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            vCam.SetActive(false);
+        }
+    }
+}
