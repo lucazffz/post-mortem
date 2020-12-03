05e7b809fad665ed73bfbc0ad0be5de800c17ded
Lucaz Lindgren <lucaz.lindgren@elev.ga.lbs.se>
2020-12-03 21:29:49 +0100

    Update

---
+++ DigDig Project/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    #region Variables
+
+    Rigidbody2D rb;
+
+    private bool canMove = true;
+
+    //X-axis movement
+    private float moveInput;
+    public float speed = 6f;
+
+    //Jump
+    public float jumpForce = 15f;
+
+    private bool isJumping;
+    public float jumpTime = 0.2f;
+    private float jumpTimeCounter;
+
+    public float jumpBuffer = 0.1f;
+    private float jumpBufferCounter;
+
+    public float hangTime = 0.2f;
+    private float hangTimeCounter;
+    
+    //ground check
+    private bool isGrounded;
+    public float checkRadius = 0.1f;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+    #endregion
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //x-axis movement
+        if (canMove)
+        {
+            moveInput = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        }
+    }
+
+    void Update()
+    {
+        #region Jump
+
+        if(canMove)
+        {
+            //ground check
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+            //hang time
+            if (isGrounded) hangTimeCounter = hangTime;
+            else hangTimeCounter -= Time.deltaTime;
+
+            //jump buffer
+            if (Input.GetButtonDown("Jump")) jumpBufferCounter = jumpBuffer;
+            else jumpBufferCounter -= Time.deltaTime;
+
+            //normal jump
+            if (hangTimeCounter > 0 && jumpBufferCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter = jumpTime;
+                jumpBufferCounter = 0;
+                isJumping = true;
+            }
+
+            //different jump height
+            if (Input.GetButton("Jump") && jumpTimeCounter > 0 && isJumping)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+                hangTimeCounter = 0;
+            }
+            else isJumping = false;
+
+            if (Input.GetButtonUp("Jump")) isJumping = false;
+        }
+        
+        #endregion
+
+
+    }
+}
