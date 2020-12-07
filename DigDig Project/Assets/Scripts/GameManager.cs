3eb17c4b444b0c1dee687a666c31fa544d27f2e2
Lucaz Lindgren <lucaz.lindgren@elev.ga.lbs.se>
2020-12-07 19:21:38 +0100

    Working - UI via game manager

---
+++ DigDig Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variables
+
+    //Variables
+
+    //interactable
+    [HideInInspector] public bool activatePrompt;
+    [HideInInspector] public KeyCode interactKey;
+    public GameObject promptText;
+
+    public string[] eventPrompt = {"open", "enter", "unlock"};
+    public int eventIndex;
+
+    #endregion
+
+    void Update()
+    {
+        //interactable
+        
+        if (activatePrompt)
+        {
+            promptText.GetComponent<Text>().text = ($"Press {interactKey} to {eventPrompt[eventIndex]}");
+
+            promptText.SetActive(true);
+        }
+        else promptText.SetActive(false);
+
+
+    }
+}
--- DigDig Project/Assets/Scripts/Interactable.cs
+++ DigDig Project/Assets/Scripts/Interactable.cs
@@ -1,42 +1,45 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class Interactable : MonoBehaviour
 {
     //Variables
-    public bool inRange;
 
-    public KeyCode interctKey = KeyCode.E;
+    private bool inRange;
+    public KeyCode interactKey = KeyCode.E;
     public UnityEvent interactionEvent;
-    public GameObject promptText;
 
-    private string[] eventType = {"open", "enter", "talk" };
-    public int eventTypeIndex;
+    public int eventIndex;
 
-    private void Start() 
-    {
-        //if (transform.parent != null && transform.parent.tag == "npc") eventTypeIndex = 2;
-        promptText.GetComponent<Text>().text = ($"Press {interctKey} to {eventType[eventTypeIndex]}");
-    }
     void Update()
     {
+        
+
         if (inRange)
         {
-            if (Input.GetKeyDown(interctKey)) interactionEvent.Invoke();
-            promptText.SetActive(true);
+            GameObject.Find("GameManager").GetComponent<GameManager>().interactKey = interactKey;
+
+            GameObject.Find("GameManager").GetComponent<GameManager>().eventIndex = eventIndex;
+
+            if (Input.GetKeyDown(interactKey)) interactionEvent.Invoke();
+            Debug.Log("Interaction");
         }
-       else promptText.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) inRange = true;
+        if (other.CompareTag("Player")) GameObject.Find("GameManager").GetComponent<GameManager>().activatePrompt = true;
+        inRange = true;
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) inRange = false;
+        if (other.CompareTag("Player")) GameObject.Find("GameManager").GetComponent<GameManager>().activatePrompt = false;
+        inRange = false;
+
+
     }
 
+    
 }
+
   
