e9180bd08ca52cc3e1c4f850f4babaf450d9972e
Lucaz Lindgren <lucaz.lindgren@elev.ga.lbs.se>
2020-12-07 21:44:25 +0100

    Working - UI on text object - Polished

---
+++ DigDig Project/Assets/Scripts/Interactable/Interactable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    //Variables
+
+    private bool inRange;
+
+    public KeyCode interactKey = KeyCode.E;
+    public int eventIndex;
+    public UnityEvent interactionEvent;
+
+    void Update()
+    {
+        //event when interacting
+        if (inRange) if (Input.GetKeyDown(interactKey)) interactionEvent.Invoke();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        inRange = true;
+
+        //UI key and event type sent to Interactable Prompt 
+        GameObject.Find("Interactable Prompt").GetComponent<InteractableUI>().interactKey = interactKey;
+        GameObject.Find("Interactable Prompt").GetComponent<InteractableUI>().eventIndex = eventIndex;
+
+        //activate UI element
+        if (other.CompareTag("Player")) GameObject.Find("Interactable Prompt").GetComponent<InteractableUI>().activatePrompt = true;
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        inRange = false;
+        //deactivate UI element
+        if (other.CompareTag("Player")) GameObject.Find("Interactable Prompt").GetComponent<InteractableUI>().activatePrompt = false; 
+    }
+
+    
+}
+
+  
---
+++ DigDig Project/Assets/Scripts/Interactable/InteractableUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InteractableUI : MonoBehaviour
+{
+    //Variables
+    [HideInInspector] public bool activatePrompt;
+    [HideInInspector] public KeyCode interactKey;
+    
+    [HideInInspector] public string[] eventPrompt = {"open", "enter", "unlock"};
+    [HideInInspector] public int eventIndex;
+
+    void Update()
+    {
+        //Set text elements
+        GetComponent<Text>().text = ($"Press {interactKey} to {eventPrompt[eventIndex]}");
+
+        //activate and deactivate text
+        if (activatePrompt) GetComponent<Text>().enabled = true;
+        else GetComponent<Text>().enabled = false;
+    }
+}
