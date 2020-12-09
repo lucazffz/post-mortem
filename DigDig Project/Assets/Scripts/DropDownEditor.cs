700c87693fbd7c74179e89302939e262c4e3ac84
Lucaz Lindgren <lucaz.lindgren@elev.ga.lbs.se>
2020-12-09 08:57:14 +0100

    Array test working

---
+++ DigDig Project/Assets/Scripts/DropDownEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(DropDown))]
+public class DropDownEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        DropDown script = (DropDown)target;
+
+        GUIContent arrayLabel = new GUIContent("MyArray");
+        script.arrayIdx = EditorGUILayout.Popup(arrayLabel, script.arrayIdx, script.MyArray);
+
+
+    }
+}
