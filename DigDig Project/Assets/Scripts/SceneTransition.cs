2beee9a3c2b870e94e5f0d6b05ef02fc34f91290
Lucaz Lindgren <74791128+LucazLindgren@users.noreply.github.com>
2020-12-16 10:59:44 +0100

    update

---
+++ DigDig Project/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    public GameObject TransitionTo, Player;
+   
+   
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            
+        }
+    }
+
+
+
+    public void Transition()
+    {
+        Player.transform.position = TransitionTo.transform.position;
+    }
+}
